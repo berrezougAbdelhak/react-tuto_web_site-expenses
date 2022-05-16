efef84385c068e85626bc3904e94c233bd3c3d60
berrezougAbdelhak <a.berrezoug@esi-sba.dz>
2022-05-16 12:14:48 +0100

    Test clickHandler

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -2,7 +2,9 @@ import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css"
 import Card from "../UI/Card";
 function ExpenseItem(props) {
-    
+    const clickHandler=()=>{
+      console.log("Clicked !!")
+    }
     
     return (
     <Card className="expense-item">
@@ -11,7 +13,7 @@ function ExpenseItem(props) {
         <h2> {props.title} </h2>
         <div className="expense-item__price"> ${props.amount}</div>
       </div>
-      <button> Change Title </button>
+      <button onClick={clickHandler}> Change Title </button>
     </Card>
   );
 }
